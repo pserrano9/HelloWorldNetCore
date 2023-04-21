9cdbe2f59e9647d822137cdfce4630892b88376b
pserrano9 <pablo.serrano@clarcat.com>
2023-04-21 16:41:09 +0200

    Unit Test Added

---
+++ HelloWOrldTestProject/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace HelloWOrldTestProject;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Equals(1, 1);
+    }
+}
