d82edee3f760e2f5f8e4cc543b41f958c07e8d30
pserrano9 <pablo.serrano@clarcat.com>
2023-04-21 16:26:16 +0200

    Forzando Errores

--- HelloWorldConsoleApp/Program.cs
+++ HelloWorldConsoleApp/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!")
+Console.WriteLine("Hello, World!");
+int zero = 0;
+int forzandoError = 3 / zero;
+
+string forzandoError2 = null;
+forzandoError2.ToString();
