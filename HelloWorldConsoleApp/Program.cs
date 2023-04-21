81697e9fdae3706ca2a9502ea6ea29531699f80b
pserrano9 <pablo.serrano@clarcat.com>
2023-04-21 16:28:04 +0200

    Error al compilar

--- HelloWorldConsoleApp/Program.cs
+++ HelloWorldConsoleApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int zero = 0;
-int forzandoError = 3 / zero;
+Console.WriteLine("Hello, World!")
 
-string forzandoError2 = null;
-forzandoError2.ToString();
+
+
